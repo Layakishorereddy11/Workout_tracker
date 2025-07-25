f68f18eb44df1cefe43dcbb0565305054620207e
Layakishore Reddy <ld786@scarletmail.rutgers.edu>
2025-07-25 14:28:24 -0400

    Building package structure, like models repositories controllers services

---
+++ src/main/java/com/workouttracker/api/models/User.java
@@ -0,0 +1,33 @@
+package com.workouttracker.api.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @CreationTimestamp
+    private Instant createdAt;
+}
