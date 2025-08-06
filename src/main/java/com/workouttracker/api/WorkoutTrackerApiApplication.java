f22f71d9a721f8d307cf157f8913d5ddb01edc6b
Layakishore Reddy <ld786@scarletmail.rutgers.edu>
2025-08-05 21:39:06 -0400

    update env

--- src/main/java/com/workouttracker/api/WorkoutTrackerApiApplication.java
+++ src/main/java/com/workouttracker/api/WorkoutTrackerApiApplication.java
@@ -2,11 +2,16 @@ package com.workouttracker.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class WorkoutTrackerApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("spring.mail.username", dotenv.get("EMAIL_USERNAME"));
+		System.setProperty("spring.mail.password", dotenv.get("EMAIL_PASSWORD"));
+
 		SpringApplication.run(WorkoutTrackerApiApplication.class, args);
 	}
 
